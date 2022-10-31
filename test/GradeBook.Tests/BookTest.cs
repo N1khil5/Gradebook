fbd59d2302b0bf0f55b990d80d46f1874bb32bec
Nikhil Suresh <nikhil@optalitix.com>
2022-10-31 13:34:39 +0000

    Gradebook project

---
+++ test/GradeBook.Tests/BookTest.cs
@@ -0,0 +1,35 @@
+using Xunit.Abstractions;
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public BookTests(ITestOutputHelper output) 
+        {
+            this.output = output;
+        }
+
+        [Fact] //attribute
+        public void Test1()
+        {
+            // Arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            // Act
+            var result = book.GetStatistics();
+
+            // Assert
+            Assert.Equal(85.7, result.Average,1);
+            output.WriteLine($"The average we expected is 85.7, the program returned {result.Average}");
+            
+            Assert.Equal(90.5, result.High,1);
+            output.WriteLine($"The maximum grade value we expected is 90.5, the program returned {result.High}");
+            Assert.Equal(77.5, result.Low,1);
+            output.WriteLine($"The lowest grade value we expected is 77.5, the program returned {result.Low}");
+        }
+    }
+}
